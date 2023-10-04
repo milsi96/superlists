6140ba4a85f917d8bd683b3f9555cf14c489e332
Mila Berni <mila.berni@agilelab.it>
2023-10-04 18:35:39 +0200

    add new-item functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
+import time
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
   
@@ -13,7 +16,29 @@ class NewVisitorTest(unittest.TestCase):
     self.browser.get('http://localhost:8000')
 
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the test!')
+    header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+    self.assertIn('To-Do', header_text)
+
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
+
+    to_do_item = 'Buy peacock feathers'
+    inputbox.send_keys(to_do_item)
+
+    inputbox.send_keys(Keys.Enter)
+    time.sleep(1)
+
+    table = self.browser.find_element(By.ID, 'id_list_table')
+    rows = table.find_element(By.TAG_NAME, 'tr')
+    self.assertTrue(
+      any(row.text == f'1: {to_do_item}' for row in rows)
+    )
+
+    self.fail('Finish the test')
+
 
 if __name__ == '__main__':
   unittest.main(warnings='ignore')
