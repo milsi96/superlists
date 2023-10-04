e61861a3ee927667599c9c483f71d83009724557
Mila Berni <mila.berni@agilelab.it>
2023-10-04 17:47:30 +0200

    add basic view

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-def home_page():
-  pass
+def home_page(request: HttpRequest):
+  return HttpResponse('<html><title>To-Do lists</title></html>')
