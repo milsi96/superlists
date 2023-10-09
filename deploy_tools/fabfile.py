ac3e6aecb7234e6eace7510f94112921ea30381e
Mila Berni <mila.berni@agilelab.it>
2023-10-10 00:10:11 +0200

    fix fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -18,7 +18,7 @@ def _get_latest_source():
   if exists('.git'):
     run('git fetch')
   else:
-    run(f'git clone {REPO_URL}')
+    run(f'git clone {REPO_URL} .')
   current_commit = local('git log -n 1 --format=%H', capture=True)
   run(f'git reset --hard {current_commit}')
 
@@ -29,14 +29,14 @@ def _update_virtual_env():
 
 def _create_or_update_dotenv():
   append('.env', 'DJANGO_DEBUG_FALSE=y')
-  append('.env', f'$SITENAME={env.host}')
+  append('.env', f'SITENAME={env.host}')
   current_contents = run('cat .env')
   if 'DJANGO_SECRET_KEY' not in current_contents:
     new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50))
     append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
-  run('.virtualenv/bin/python manage.py collectstatic --noinput')
+  run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
 def _update_database():
-  run('.virtualenv/bin/python manage.py migrate --noinput')
+  run('./virtualenv/bin/python manage.py migrate --noinput')
