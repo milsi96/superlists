52cd9f5a0dcc9acafdffc8992ba6127e6f60d122
Mila Berni <mila.berni@agilelab.it>
2023-10-12 23:15:18 +0200

    add final touches to duplicate validation

--- lists/forms.py
+++ lists/forms.py
@@ -3,7 +3,7 @@ from lists.models import Item
 
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = "You've already for this in your list"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -36,3 +36,6 @@ class ExistingListItemForm(ItemForm):
     except forms.ValidationError as e:
       e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
       self._update_errors(e)
+
+  def save(self):
+    return forms.models.ModelForm.save(self)
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -23,17 +23,23 @@ class ItemFormTest(TestCase):
     self.assertEqual(new_item.text, 'do me')
     self.assertEqual(new_item.list, list_)
 
+  def test_form_save(self):
+    list_ = List.objects.create()
+    form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+    new_item = form.save()
+    self.assertEqual(new_item, Item.objects.all()[0])
+
 
 class ExistingListItemFormTest(TestCase):
 
   def test_form_renders_item_text_input(self):
     list_ = List.objects.create()
     form = ExistingListItemForm(for_list=list_)
-    self.asseertIn('placeholder="Enter a to-do item', form.as_p())
+    self.assertIn('placeholder="Enter a to-do item', form.as_p())
 
   def test_form_validation_for_blank_items(self):
     list_ = List.objects.create()
-    form = ExistingListItemForm(for_list=list_, data={'text', ''})
+    form = ExistingListItemForm(for_list=list_, data={'text': ''})
     self.assertFalse(form.is_valid())
     self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
