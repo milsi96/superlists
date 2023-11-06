79e47c28ec36eca92876084b87a6c454636beca6
Mila Berni <mila.berni@agilelab.it>
2023-11-07 00:45:26 +0100

    add new list view

--- lists/urls.py
+++ lists/urls.py
@@ -5,5 +5,5 @@ from lists import views
 urlpatterns = [
     path('<int:list_id>/', views.view_list, name='view_list'),
     path('new', views.new_list, name='new_list'),
-    path('users/<str:email>', views.my_lists, name='my_lists'),
+    path('users/<str:email>/', views.my_lists, name='my_lists'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,8 @@ from django.forms import ValidationError
 from django.shortcuts import render, redirect
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 def home_page(request):
   return render(request, 'home.html', {'form': ItemForm()})
@@ -22,10 +24,13 @@ def new_list(request):
   form = ItemForm(data=request.POST)
   if form.is_valid():
     list_ = List.objects.create()
+    list_.owner = request.user
+    list_.save()
     form.save(for_list=list_)
     return redirect(list_)
   else:
     return render(request, 'home.html', {'form': form})
 
 def my_lists(request, email):
-  return render(request, 'my_lists.html')
+  owner = User.objects.get(email=email)
+  return render(request, 'my_lists.html', {'owner': owner})
