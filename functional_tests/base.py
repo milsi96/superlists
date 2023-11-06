fb5439722205d4760a6afd8492ef7427df1303b3
Mila Berni <mila.berni@agilelab.it>
2023-11-07 00:26:01 +0100

    add my lists template, view and url

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 
 MAX_WAIT: int = 10
@@ -26,6 +27,14 @@ class FuncionalTest(StaticLiveServerTestCase):
 
   def get_item_input_box(self):
     return self.browser.find_element(By.ID, 'id_text')
+
+
+  def add_list_item(self, item_text):
+    num_rows = len(self.browser.find_elements(By.CSS_SELECTOR, '#id_list_table tr'))
+    self.get_item_input_box().send_keys(item_text)
+    self.get_item_input_box().send_keys(Keys.ENTER)
+    item_number = num_rows + 1
+    self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
 
   def setUp(self):
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -5,6 +5,7 @@ from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.conf import settings
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
+from selenium.webdriver.common.by import By
 User = get_user_model()
 
 
@@ -24,12 +25,46 @@ class MyListsTest(FuncionalTest):
       path='/'
     ))
 
-  def test_logged_in_users_are_saved_as_my_lists(self):
-    email = 'edit@example.com'
-    self.browser.get(self.live_server_url)
-    self.wait_to_be_logged_out(email)
+  def test_logged_in_users_lists_are_saved_as_my_lists(self):
+    # Edith is a logged in user
+    self.create_pre_authenticated_session('edith@example.com')
 
-    # Edith is a logged-in user
-    self.create_pre_authenticated_session(email)
+    # She goes to the home page and starts a list
     self.browser.get(self.live_server_url)
-    self.wait_to_be_logged_in(email)
+    self.add_list_item('Reticulate splines')
+    self.add_list_item('Immanentize eschaton')
+    first_list_url = self.browser.current_url
+
+    # She notices a "My Lists" link, for the first time.
+    self.browser.find_element(By.LINK_TEXT, 'My Lists').click()
+
+    # She sees that her list is in there, named according to its
+    # first list item
+    self.wait_for(
+      lambda: self.browser.find_element(By.LINK_TEXT, 'Reticulate splines')
+    )
+    self.browser.find_element(By.LINK_TEXT, 'Reticulate splines').click()
+    self.wait_for(
+      lambda: self.assertEqual(self.browser.current_url, first_list_url)
+    )
+
+    # She decides to start another list, just to see
+    self.browser.get(self.live_server_url)
+    self.add_list_item('Click cows')
+    second_list_url = self.browser.current_url
+
+    # Under "my lists", her new list appears
+    self.browser.find_element(By.LINK_TEXT, 'My Lists').click()
+    self.wait_for(
+      lambda: self.browser.find_element(By.LINK_TEXT, 'Click cows').click()
+    )
+    self.wait_for(
+      lambda: self.assertEqual(self.browser.current_url, second_list_url)
+    )
+
+    # She logs out. The "My Lists" option disappears
+    self.browser.find_element(By.LINK_TEXT, 'Log out').click()
+    self.wait_for(lambda: self.assertEqual(
+      self.browser.find_elements(By.LINK_TEXT, 'My Lists'),
+      []
+    ))
